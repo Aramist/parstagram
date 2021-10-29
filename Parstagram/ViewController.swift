40cd63ee9ee465eba7c3baad2f1ca2841504e843
Aramis Tanelus <aramis.tanelus@nyu.edu>
2021-10-29 17:02:14 -0400

    Initialize cocoapods

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Aramis on 10/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
